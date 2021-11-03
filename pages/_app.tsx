4ccce928dab14e2097668716db442f92059333cd
Harsha Srikara <harshasrikara@gmail.com>
2021-11-03 18:35:16 -0500

    refactor: convert core components / pages to typescript, merge in from feat/mongoose

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import "styles/globals.css";
-import type { AppProps } from "next/app";
-import "reflect-metadata";
+import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "styles/tailwind.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: any) {
+  const Layout = Component.layout || (({ children }: any) => <>{children}</>);
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  );
 }
-export default MyApp;
