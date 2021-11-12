e9feed3319c210da33a875a83c749a8717765b7a
spenpal2000 <sanjeev.penupala@gmail.com>
2021-11-11 23:51:21 -0600

    feat(upload): add function to upload gcode files to octoprint
    feat #2

---
+++ util/upload.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+const FormData = require("form-data");
+
+function upload(file: any, filename: string) {
+  let form = new FormData();
+
+  form.append("file", file, {
+    filepath: filename,
+  });
+
+  const config = {
+    baseURL: "http://localhost:80/api/",
+    headers: {
+      ...form.getHeaders(),
+      "X-Api-Key": "31331488D0734D6D9AA373F7C644D037",
+      "Content-Length": form.getLengthSync(),
+    },
+  };
+
+  axios
+    .post("/files/local", form, config)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
